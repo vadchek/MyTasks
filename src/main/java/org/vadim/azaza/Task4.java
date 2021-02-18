e5b279209ac71c698c6606357a1ec03ce8d32bc5
Vadim Chekotin <vadchek93@gmail.com>
2021-02-19 01:40:50 +0300

    commit1

--- src/main/java/org/vadim/azaza/Task4.java
+++ src/main/java/org/vadim/azaza/Task4.java
@@ -1,28 +1,21 @@
 package org.vadim.azaza;
 
 public class Task4 {
-    public static void main(String[] args){
-        int num = 307, i, j, k;
+    public static void main(String[] args) {
+        int num = 2876, i, k, dec, max;
         k = num;
+        max = 0;
+        dec = 1;
+        for (dec = 1; dec <= num; dec *= 10) {}
 
-        for(i = 0; k >= 100; i++)
-            k = k - 100;
+        for (dec = dec/10; dec > 0; dec /= 10) {
+            for (i = 0; k >= dec; i++)
+                k = k - dec;
 
-        for(j = 0; k >= 10; j++)
-            k = k - 10;
-
-        if(i > j){
-            if(i > k)
-                System.out.println("Of number " + num + " the largest digit is " + i);
-            else
-                System.out.println("Of number " + num + " the largest digit is " + k);
-        }
-        else{
-            if(j > k)
-                System.out.println("Of number " + num + " the largest digit is " + j);
-            else
-                System.out.println("Of number " + num + " the largest digit is " + k);
+            if (i > max)
+                max = i;
         }
 
+        System.out.println("Of number " + num + " the largest digit is " + max);
     }
 }
